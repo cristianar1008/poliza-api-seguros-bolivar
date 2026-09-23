4fc458f73af6934b8eff4401c92935db89a87061
cristianar1008 <cristianpar.2002@gmail.com>
2026-09-22 23:22:47 -0500

    Estructura inicial del proyecto Spring Boot

---
+++ poliza-api-seguros-bolivar/poliza-api-seguros-bolivar/src/main/java/com/segurosbolivar/polizaapi/PolizaApiSegurosBolivarApplication.java
@@ -0,0 +1,13 @@
+package com.segurosbolivar.polizaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolizaApiSegurosBolivarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolizaApiSegurosBolivarApplication.class, args);
+    }
+
+}
---
+++ poliza-api-seguros-bolivar/poliza-api-seguros-bolivar/src/test/java/com/segurosbolivar/polizaapi/PolizaApiSegurosBolivarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segurosbolivar.polizaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolizaApiSegurosBolivarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
